ae5ee0893bab2bf914023a15ec0962fb1bdfef81
Hp <40killer40@mail.ru>
2023-06-11 23:37:39 +0300

    add description

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from datetime import *
 
 
 def load_operations():
+    """Функция загружает из файла данные о транзакциях"""
     with open("operations.json", encoding="utf-8") as operations:
         return json.load(operations)
 
 
+"Пересобираю список без пустого словаря"
 operations_wo_empty_dict = [transaction for transaction in load_operations() if transaction]
 
 
 def operation_sort(collection):
+    """Функция сортирует список с информацией о транзакциях по дате и выводит в требуемом для выполниния задания
+    формате информацию о последних пяти"""
 
+    "Сортировка по дате"
     sorted_operations = sorted(collection, key=lambda x: datetime.strptime(x['date'][:-7], '%Y-%m-%dT%H:%M:%S'), reverse=True)
 
-    print(sorted_operations[:5])
-
+    "Парсинг вывода"
     for operation in sorted_operations[:5]:
         print(f'{".".join(operation["date"][:10].split("-")[::-1])} {operation["description"]}')
         sendler = operation.get('from', '')
@@ -28,7 +32,7 @@ def operation_sort(collection):
             print(*sendler.split()[:-1], end=' ')
             [print(hidden_sendler_number[i:i + 4], end=" ") for i in range(0, 13, 4)]
             print('-> ', end='')
-        if reciver[0] == ("Счет"):
+        if reciver[0] == 'Счет':
             print(f'Счет **{reciver[1][-4:]}')
         else:
             hidden_reciver_number = reciver[-1][:6] + '*'*6 + reciver[-1][-4:]
@@ -37,4 +41,5 @@ def operation_sort(collection):
             print()
         print(f'{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}', end='\n\n')
 
+    "Добавил ретёрн для тестов"
     return sorted_operations[:5]
--- tests/test_sort_test.py
+++ tests/test_sort_test.py
@@ -1,6 +1,7 @@
 from main import operation_sort, operations_wo_empty_dict
 
 def test_operation_sort():
+    "Функция тестирует сортировк по датам 5 последних элементов, написана с использованием pytest"
     assert_list = [{'id': 863064926, 'state': 'EXECUTED', 'date': '2019-12-08T22:46:21.935582', 'operationAmount': {'amount': '41096.24', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Открытие вклада', 'to': 'Счет 90424923579946435907'},
                    {'id': 114832369, 'state': 'EXECUTED', 'date': '2019-12-07T06:17:14.634890', 'operationAmount': {'amount': '48150.39', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Visa Classic 2842878893689012', 'to': 'Счет 35158586384610753655'},
                    {'id': 560813069, 'state': 'CANCELED', 'date': '2019-12-03T04:27:03.427014', 'operationAmount': {'amount': '17628.50', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'MasterCard 1796816785869527', 'to': 'Visa Classic 7699855375169288'},
