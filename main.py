db62e1dae742aa2584b2caafe38b8d90a8288653
Hp <40killer40@mail.ru>
2023-06-16 23:29:06 +0300

    fixes 4 operation state

--- main.py
+++ main.py
@@ -18,28 +18,36 @@ def operation_sort(collection):
 
     "Сортировка по дате"
     sorted_operations = sorted(collection, key=lambda x: datetime.strptime(x['date'][:-7], '%Y-%m-%dT%H:%M:%S'), reverse=True)
-
+    counter = 0
     "Парсинг вывода"
-    for operation in sorted_operations[:5]:
-        print(f'{".".join(operation["date"][:10].split("-")[::-1])} {operation["description"]}')
-        sendler = operation.get('from', '')
-        reciver = operation["to"].split()
+    for operation in sorted_operations:
 
-        if sendler and sendler.startswith('Счет'):
-            print(f'Счет **{sendler[-4:]}', end=' -> ')
-        elif sendler:
-            hidden_sendler_number = sendler[-16:-10] + '*' * 6 + sendler[-4:]
-            print(*sendler.split()[:-1], end=' ')
-            [print(hidden_sendler_number[i:i + 4], end=" ") for i in range(0, 13, 4)]
-            print('-> ', end='')
-        if reciver[0] == 'Счет':
-            print(f'Счет **{reciver[1][-4:]}')
-        else:
-            hidden_reciver_number = reciver[-1][:6] + '*'*6 + reciver[-1][-4:]
-            print(*reciver[:-1], end=' ')
-            [print(hidden_reciver_number[i:i + 4], end=" ")for i in range(0, 13, 4)]
-            print()
-        print(f'{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}', end='\n\n')
+        if all([counter <= 5, operation['state'] == 'EXECUTED']):
+            print(f'{".".join(operation["date"][:10].split("-")[::-1])} {operation["description"]}')
+            sendler = operation.get('from', '')
+            reciver = operation["to"].split()
+
+            if sendler and sendler.startswith('Счет'):
+                print(f'Счет **{sendler[-4:]}', end=' -> ')
+            elif sendler:
+                hidden_sendler_number = sendler[-16:-10] + '*' * 6 + sendler[-4:]
+                print(*sendler.split()[:-1], end=' ')
+                [print(hidden_sendler_number[i:i + 4], end=" ") for i in range(0, 13, 4)]
+                print('-> ', end='')
+            if reciver[0] == 'Счет':
+                print(f'Счет **{reciver[1][-4:]}')
+            else:
+                hidden_reciver_number = reciver[-1][:6] + '*' * 6 + reciver[-1][-4:]
+                print(*reciver[:-1], end=' ')
+                [print(hidden_reciver_number[i:i + 4], end=" ") for i in range(0, 13, 4)]
+                print()
+            print(f'{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}',
+            end='\n\n')
+            counter += 1
+        if counter == 5:
+            break
 
     "Добавил ретёрн для тестов"
     return sorted_operations[:5]
+
+operation_sort(operations_wo_empty_dict)
